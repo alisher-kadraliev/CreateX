3ff9820033ebd1287f246591508fc0f3800dec2a
Alisher <98706585+alisher-kadraliev@users.noreply.github.com>
2022-08-07 00:36:49 +0300

    header added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,80 @@
 // Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    const info = document.querySelector('.info')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+            info.classList.add('active')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+            info.classList.remove('active')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+            info.classList.remove('active')
+        }
+    })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+    const nav = document.querySelector('nav')
+
+    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+    const breakpoint = 1
+    if (window.scrollY >= breakpoint) {
+        nav.classList.add('fixed__nav')
+    } else {
+        nav.classList.remove('fixed__nav')
+    }
+}
+window.addEventListener('scroll', fixedNav)
+
+
+const swiper = new Swiper('.swiper__team', {
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    //   // Responsive breakpoints
+    //   breakpoints: {
+    //   // when window width is >= 320px
+    //   320: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 20
+    //   },
+    //   // when window width is >= 480px
+    //   480: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30
+    //   },
+    // }
+});
