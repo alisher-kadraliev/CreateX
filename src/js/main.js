bd4f47308e97a9e1b444908958ea4bc1f3d73fa3
Alisher <98706585+alisher-kadraliev@users.noreply.github.com>
2022-08-20 12:54:58 +0300

    Services page end and Services-2 start

--- src/js/main.js
+++ src/js/main.js
@@ -62,8 +62,26 @@ $(document).ready(function () {
         slidesToScroll: 2,
     });
     $(".clients__swiper").slick({
-        arrows:true,
-        
+        arrows: true,
+
     });
 });
 
+// Аккордеон
+function accordion() {
+    const items = document.querySelectorAll('.accordion__item-trigger')
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            if (parent.classList.contains('accordion__item-active')) {
+                parent.classList.remove('accordion__item-active')
+            } else {
+                document
+                    .querySelectorAll('.accordion__item')
+                    .forEach(child => child.classList.remove('accordion__item-active'))
+                parent.classList.add('accordion__item-active')
+            }
+        })
+    })
+}
+accordion() 
